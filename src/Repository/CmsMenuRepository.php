85acade3169dadfb1f983d100fcda52dcc1ab289
Clement <clement@webetdesign.com>
2019-02-11 18:22:44 +0100

    Bug fix repo

--- src/Repository/CmsMenuRepository.php
+++ src/Repository/CmsMenuRepository.php
@@ -2,8 +2,10 @@
 
 namespace WebEtDesign\CmsBundle\Repository;
 
+use Symfony\Bridge\Doctrine\RegistryInterface;
 use WebEtDesign\CmsBundle\Entity\CmsMenu;
 use Gedmo\Tree\Entity\Repository\NestedTreeRepository;
+use LogicException;
 
 /**
  * @method CmsMenu|null find($id, $lockMode = null, $lockVersion = null)
@@ -13,6 +15,20 @@ use Gedmo\Tree\Entity\Repository\NestedTreeRepository;
  */
 class CmsMenuRepository extends NestedTreeRepository
 {
+
+    public function __construct(RegistryInterface $registry) {
+        $manager = $registry->getManagerForClass(CmsMenu::class);
+
+        if ($manager === null) {
+            throw new LogicException(sprintf(
+                'Could not find the entity manager for class "%s". Check your Doctrine configuration to make sure it is configured to load this entity’s metadata.',
+                CmsMenu::class
+            ));
+        }
+
+        parent::__construct($manager, $manager->getClassMetadata(CmsMenu::class));
+    }
+
     public function getRootByName($rootName)
     {
         $qb = $this->createQueryBuilder('m')
