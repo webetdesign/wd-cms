7d775e01d3aba5de6eea9f576df9908ec597fa9e
Clement <clement@webetdesign.com>
2019-02-14 12:15:08 +0100

    config js css

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path')
+const ENV = process.env.APP_ENV;
+const isTest = ENV === 'test'
+const isProd = ENV === 'prod';
+
+function setDevTool () {
+  if (isTest) {
+    return 'inline-source-map';
+  } else if (isProd) {
+    return 'source-map';
+  } else {
+    return 'eval-source-map';
+  }
+}
+
+const config = {
+  entry: path.resolve('./assets/js/app.js'),
+  output: {
+    path: path.resolve('./src/Resources/public'),
+    filename: 'cms_admin.js',
+  },
+  devtool: setDevTool(),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: [
+          /node_modules/
+        ]
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: ExtractTextWebpackPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextWebpackPlugin("cms_admin.css")
+  ]
+};
+
+module.exports = config;
