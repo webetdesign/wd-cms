4b975c91138685287d1aae4113aeeb3aa91c5d60
benjamin <emailpourfb90@gmail.com>
2019-09-12 11:26:02 +0200

    edit text front

--- assets/js/front.js
+++ assets/js/front.js
@@ -1,7 +1,5 @@
 import '../sass/cms_front.scss'
 import $ from "jquery"
-global.$ = global.jQuery = $
-
 
 $(document).ready(function() {
     function lauchModalEditContent(id) {
@@ -10,50 +8,17 @@ $(document).ready(function() {
 
         if (id){
             $.post('admin/webetdesign/cms/cmscontent/'+ id +'/edit', function( response ) {
-                var modal = $("#modalEditContentBody");
-                modal.html(response)
+                $("#modalEditContentBody").html(response)
 
-                var modalForm = modal.find('form')[0];
-
-                var uniqid = modalForm[0].name.substring(0, modalForm[0].name.indexOf('['));
-
-
-                modalForm.onsubmit=function(e) {
+                $("#modalEditContentBody").find('form')[0].onsubmit=function(e) {
                     e.preventDefault();
 
-                    if (Object.entries(CKEDITOR.instances).length){
-                        for(var instanceName in CKEDITOR.instances)
-                            CKEDITOR.instances[instanceName].updateElement();
-                    }
+                    let form = $(e.target).serialize()
 
-                    let form = $(e.target).serialize();
-                    var form_value = $(e.target).serializeArray();
-                    console.log(e.target);
+                    var uniqid = e.target[0].name.substring(0, e.target[0].name.indexOf('['))
 
-                    $.post('admin/webetdesign/cms/cmscontent/'+ id +'/edit?uniqid=' + uniqid, form).done(function() {
-                        $(form_value).each(function(index, element ) {
-                            if (element.name === uniqid + '[value]'){
-                                var chev = (element.value).indexOf(">");
 
-                                if (!chev || element.value[0] !== '<'){
-                                    var value = "<span class='text-edit-content' data-btn='" + id + "'>" + element.value + "</span>";
-                                }else{
-                                    var value = (element.value).substring(0, chev ) + " class='text-edit-content' data-btn='" + id + "'>" + (element.value).substring(chev + 1 );
-                                }
-
-                                var divContent = $("#div-content-" + id);
-                                $(divContent).html(value)
-
-                                $(divContent[0].firstChild).hover( function(e) {
-                                    var btn = $("#div-content-" + id)[0].firstChild.dataset.btn;
-
-                                    $("#btn-edit-content-" + btn).show().delay(2000).fadeOut();
-                                })
-
-                                modal.prepend('<div class="alert alert-success" role="alert">Modification effectuée</div>')
-                            }
-                        })
-                    })
+                    $.post('admin/webetdesign/cms/cmscontent/'+ id +'/edit?uniqid=' + uniqid, form)
 
                 }
 
@@ -62,17 +27,11 @@ $(document).ready(function() {
         }
     }
 
-    function printLoader(){
-        $("#modalEditContentBody").html('<div style="text-align: center; width: 100%">\n' +
-            '    <i class="fa fa-spinner fa-4x fa-spin" aria-hidden="true"></i>\n' +
-            '</div>'
-        )
-    }
-
     var classname = document.getElementsByClassName("open-modal-edit-content");
 
     for (var i = 0; i < classname.length; i++) {
         classname[i].addEventListener('click', function(e) {
+            // console.log(e.target.next());
             lauchModalEditContent(e.target.dataset["id"])
         }, false);
     }
@@ -85,25 +44,11 @@ $(document).ready(function() {
 // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
-        $("button[id^='btn-edit-content']").hide();
-        printLoader()
     }
 
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
-            $("button[id^='btn-edit-content']").hide();
-            printLoader()
         }
     }
-
-    $(".text-edit-content").hover( function(e) {
-        if (!e.target.dataset.btn){
-            var btn = e.target.parentNode.dataset.btn;
-        }
-        var btn = e.target.dataset.btn;
-
-        $("#btn-edit-content-" + btn).show().delay(2000).fadeOut();
-    })
-
 })
--- src/Twig/CmsTwigExtension.php
+++ src/Twig/CmsTwigExtension.php
@@ -2,10 +2,8 @@
 
 namespace WebEtDesign\CmsBundle\Twig;
 
-use App\Entity\User;
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 use Twig\Environment;
 use WebEtDesign\CmsBundle\Entity\CmsContent;
 use WebEtDesign\CmsBundle\Entity\CmsContentHasSharedBlock;
@@ -32,7 +30,6 @@ class CmsTwigExtension extends AbstractExtension
     protected $router;
 
     protected $customContents;
-    private   $tokenStorage;
 
     /**
      * @inheritDoc
@@ -43,8 +40,7 @@ class CmsTwigExtension extends AbstractExtension
         $customContents,
         Container $container,
         Environment $twig,
-        TemplateProvider $templateProvider,
-        TokenStorage $tokenStorage
+        TemplateProvider $templateProvider
     ) {
         $this->em                  = $entityManager;
         $this->router              = $router;
@@ -52,7 +48,6 @@ class CmsTwigExtension extends AbstractExtension
         $this->container           = $container;
         $this->twig                = $twig;
         $this->sharedBlockProvider = $templateProvider;
-        $this->tokenStorage = $tokenStorage;
     }
 
 
@@ -150,30 +145,14 @@ class CmsTwigExtension extends AbstractExtension
     }
 
     public function renderContent($content, $type = "text"){
-        /** @var User $user */
-        $user = $this->tokenStorage->getToken()->getUser();
-
-
-        if ($user != "anon." && $user->hasRole('ROLE_ADMIN')){
-            $value = "<button style='display: none' id='btn-edit-content-" . $content->getId() . "' data-id='" . $content->getId() . "'  class='open-modal-edit-content'>";
-            $value .= "<i  data-id='" . $content->getId() . "' class='fa fa-edit'></i></button>";
-            $value .= $this->addClassToContent($content->getValue(), $content->getId());
+        if (true){
+            $value = "<button data-id='" . $content->getId() . "'  class='open-modal-edit-content'><i  data-id='" . $content->getId() . "' class='fa fa-edit'></i></button>" . $content->getValue();
         }else{
-            $value = $content->getValue();
+            $value = $content;
         }
+
 
         return $value;
-    }
-
-    private function addClassToContent($content, $id){
-        $chev = strpos($content, ">");
-
-        if (!$chev || $content[0] !== '<'){
-            $value = "<span class='text-edit-content' data-btn='" . $id . "'>" . $content . "</span>";
-        }else{
-            $value = substr($content, 0, $chev ) . " class='text-edit-content' data-btn='" . $id . "'>" . substr($content, $chev + 1 );
-        }
-        return "<div id='div-content-" . $id . "'>" . $value . "</div>";
     }
 
     public function renderSharedBlock(CmsSharedBlock $block)
