06ff0d436e7d18a970614125d11ca1ffcb3fdaa2
clement <clement@webetdesign.com>
2019-12-19 15:07:23 +0100

    add listener to warmup route cache whene cmsRoute are update in admin

---
+++ src/EventListener/RouteAdminListener.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace WebEtDesign\CmsBundle\EventListener;
+
+
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpKernel\KernelInterface;
+use WebEtDesign\CmsBundle\Entity\CmsRoute;
+
+class RouteAdminListener
+{
+    /**
+     * @var Router
+     */
+    protected $router;
+    /**
+     * @var Filesystem
+     */
+    protected $fs;
+    /**
+     * @var KernelInterface
+     */
+    protected $kernel;
+
+
+    /**
+     * RouteAdminListener constructor.
+     * @param Router $router
+     * @param Filesystem $fs
+     * @param KernelInterface $kernel
+     */
+    public function __construct(Router $router, Filesystem $fs, KernelInterface $kernel) {
+        $this->router    = $router;
+        $this->fs        = $fs;
+        $this->kernel    = $kernel;
+    }
+
+    public function postUpdate(LifecycleEventArgs $event)
+    {
+        $route = $event->getObject();
+
+        if (!$route instanceof CmsRoute) {
+            return;
+        }
+
+        $this->warmUpRouteCache();
+    }
+
+
+    protected function warmUpRouteCache()
+    {
+        $cacheDir = $this->kernel->getCacheDir();
+
+        foreach (['matcher_cache_class', 'generator_cache_class'] as $option) {
+            $className = $this->router->getOption($option);
+            $cacheFile = $cacheDir . DIRECTORY_SEPARATOR . $className . '.php';
+            $this->fs->remove($cacheFile);
+        }
+
+        $this->router->warmUp($cacheDir);
+    }
+}
