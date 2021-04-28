c54273653331d8deadfe7c408b0c8aa67ea04aa9
clement <clement@webetdesign.com>
2021-04-28 16:38:09 +0200

    command update page params change controller name and method

--- src/Command/CmsUpdatePageParamsCommand.php
+++ src/Command/CmsUpdatePageParamsCommand.php
@@ -10,6 +10,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use WebEtDesign\CmsBundle\Entity\AbstractCmsRoute;
 use WebEtDesign\CmsBundle\Entity\CmsContent;
 use WebEtDesign\CmsBundle\Entity\CmsPage;
 use WebEtDesign\CmsBundle\Entity\CmsPageDeclination;
@@ -31,8 +32,11 @@ class CmsUpdatePageParamsCommand extends AbstractCmsUpdateContentsCommand
     protected $pageRp;
 
 
-    public function __construct(string $name = null, EntityManagerInterface $em, TemplateProvider $pageProvider)
-    {
+    public function __construct(
+        string $name = null,
+        EntityManagerInterface $em,
+        TemplateProvider $pageProvider
+    ) {
         parent::__construct($name, $em, $pageProvider);
     }
 
@@ -49,7 +53,7 @@ class CmsUpdatePageParamsCommand extends AbstractCmsUpdateContentsCommand
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->init($input, $output);
-        $this->pageRp    = $this->em->getRepository(CmsPage::class);
+        $this->pageRp = $this->em->getRepository(CmsPage::class);
 
         if ($input->getOption('all')) {
             if ($this->io->confirm('Resetting all page\' configuration, are you sure to continue')) {
@@ -107,28 +111,47 @@ class CmsUpdatePageParamsCommand extends AbstractCmsUpdateContentsCommand
             return false;
         }
 
-        if (!$page->getRoute()) return false;
+        if (!$page->getRoute()) {
+            return false;
+        }
 
         $this->updateParams($page->getRoute(), $config);
+
+        $this->updateRouteMetadata($page->getRoute(), $config);
 
         return true;
     }
 
-    private function updateParams(CmsRouteInterface $cmsRoute, $config){
+    private function updateRouteMetadata(CmsRouteInterface $route, $config)
+    {
+        $route->setController($config['controller'] . '::' . $config['action']);
+
+        $route->setMethods($config['methods']);
+
+        if (!empty($config['route'])) {
+            $route->setName($config['route']);
+        }
+
+        return $route;
+    }
+
+    private function updateParams(CmsRouteInterface $cmsRoute, $config)
+    {
         preg_match('/{.*}/', $cmsRoute->getPath(), $defined);
         $config = isset($config['params']) ? $config['params'] : [];
 
         foreach ($defined as $item) {
             $param = str_replace(['{', '}'], '', $item);
-            if (!array_key_exists($param, $config)){
+            if (!array_key_exists($param, $config)) {
                 $cmsRoute = $this->removeParam($cmsRoute, $param);
             }
         }
 
         foreach ($config as $param => $item) {
-            if (strpos($cmsRoute->getPath(), $param) < 0 || !strpos($cmsRoute->getPath(), $param)){
+            if (strpos($cmsRoute->getPath(), $param) < 0 || !strpos($cmsRoute->getPath(), $param)) {
                 $cmsRoute = $this->addParam($cmsRoute, $param, $config[$param]);
             }
+            $this->upsertDefaultAndRequirement($cmsRoute, $param, $config[$param]);
         }
 
         $cmsRoute->setPath(
@@ -138,35 +161,42 @@ class CmsUpdatePageParamsCommand extends AbstractCmsUpdateContentsCommand
         $this->em->persist($cmsRoute);
     }
 
-    private function removeParam(CmsRouteInterface $cmsRoute, $param){
+    private function removeParam(CmsRouteInterface $cmsRoute, $param)
+    {
         $cmsRoute->setPath(
             str_replace('/{' . $param . '}', '', $cmsRoute->getPath())
         );
 
         $defaults = json_decode($cmsRoute->getDefaults(), true);
-        if (isset($defaults[$param])){
+        if (isset($defaults[$param])) {
             unset($defaults[$param]);
         }
         $cmsRoute->setDefaults(json_encode($defaults));
         $requirements = json_decode($cmsRoute->getRequirements(), true);
-        if (isset($requirements[$param])){
+        if (isset($requirements[$param])) {
             unset($requirements[$param]);
         }
         $cmsRoute->setRequirements(json_encode($requirements));
         return $cmsRoute;
     }
 
-    private function addParam(CmsRouteInterface $cmsRoute, $param, $config){
+    private function addParam(CmsRouteInterface $cmsRoute, $param, $config)
+    {
         $cmsRoute->setPath($cmsRoute->getPath() . '/{' . $param . '}');
 
-       if (isset($config['default'])){
-           $defaults = json_decode($cmsRoute->getDefaults(), true);
-           $defaults[$param] = $config['default'];
-           $cmsRoute->setDefaults(json_encode($defaults));
-       }
+        return $cmsRoute;
+    }
 
-        if (isset($config['requirement'])){
-            $requirements = json_decode($cmsRoute->getRequirements(), true);
+    private function upsertDefaultAndRequirement(CmsRouteInterface $cmsRoute, $param, $config)
+    {
+        if (isset($config['default'])) {
+            $defaults         = json_decode($cmsRoute->getDefaults(), true);
+            $defaults[$param] = $config['default'];
+            $cmsRoute->setDefaults(json_encode($defaults));
+        }
+
+        if (isset($config['requirement'])) {
+            $requirements         = json_decode($cmsRoute->getRequirements(), true);
             $requirements[$param] = $config['requirement'];
             $cmsRoute->setRequirements(json_encode($requirements));
         }
