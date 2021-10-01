1ad9eb467363a41f1f25b623f4037381c1f90896
clement <clement@webetdesign.com>
2021-10-01 10:29:03 +0200

    config seo bundle

---
+++ src/Sitemap/SitemapSubscriber.php
@@ -0,0 +1,114 @@
+<?php
+
+
+namespace WebEtDesign\CmsBundle\Sitemap;
+
+
+use Doctrine\ORM\EntityManagerInterface;
+use Presta\SitemapBundle\Event\SitemapPopulateEvent;
+use Presta\SitemapBundle\Service\UrlContainerInterface;
+use Presta\SitemapBundle\Sitemap\Url\GoogleMultilangUrlDecorator;
+use Presta\SitemapBundle\Sitemap\Url\UrlConcrete;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use WebEtDesign\CmsBundle\Entity\CmsRoute;
+use WebEtDesign\CmsBundle\Repository\CmsSiteRepository;
+use WebEtDesign\CmsBundle\Services\TemplateProvider;
+
+class SitemapSubscriber implements EventSubscriberInterface
+{
+
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private UrlGeneratorInterface  $urlGenerator;
+    private CmsSiteRepository      $cmsSiteRepository;
+    private ParameterBagInterface  $parameterBag;
+
+    /**
+     * @param UrlGeneratorInterface $urlGenerator
+     * @param CmsSiteRepository $cmsSiteRepository
+     * @param ParameterBagInterface $parameterBag
+     */
+    public function __construct(
+        UrlGeneratorInterface $urlGenerator,
+        CmsSiteRepository $cmsSiteRepository,
+        ParameterBagInterface $parameterBag
+    ) {
+        $this->urlGenerator      = $urlGenerator;
+        $this->cmsSiteRepository = $cmsSiteRepository;
+        $this->parameterBag      = $parameterBag;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            SitemapPopulateEvent::ON_SITEMAP_POPULATE => 'populate',
+        ];
+    }
+
+    /**
+     * @param SitemapPopulateEvent $event
+     */
+    public function populate(SitemapPopulateEvent $event): void
+    {
+        $this->registerCmsPages($event->getUrlContainer());
+    }
+
+    /**
+     * @param UrlContainerInterface $urls
+     */
+    public function registerCmsPages(UrlContainerInterface $urls): void
+    {
+        $cms_config = $this->parameterBag->get('wd_cms.cms');
+
+        $sites = $this->cmsSiteRepository->findAll();
+
+        foreach ($sites as $site) {
+            if ($site->getHost() !== null) {
+                $context = $this->urlGenerator->getContext();
+                $context->setHost($site->getHost());
+                $this->urlGenerator->setContext($context);
+            }
+
+            $pages = $site->getPages();
+
+            foreach ($pages as $page) {
+                /** @var CmsRoute $route */
+                $route = $page->getRoute();
+                if ($page->isActive() && $route) {
+                    if (!$route->isDynamic()) {
+                        $url = new UrlConcrete(
+                            $this->urlGenerator->generate(
+                                $route->getName(),
+                                [],
+                                UrlGeneratorInterface::ABSOLUTE_URL
+                            ),
+                            $page->getUpdatedAt()
+                        );
+
+                        $decoratedUrl = new GoogleMultilangUrlDecorator($url);
+
+
+                        if ($cms_config['multilingual']) {
+
+                            foreach ($page->getCrossSitePages() as $crossSitePage) {
+                                $crossRoute = $crossSitePage->getRoute();
+                                if ($crossRoute && !$crossRoute->isDynamic()) {
+                                    $decoratedUrl->addLink($this->urlGenerator->generate(
+                                        $crossRoute->getName(),
+                                        [],
+                                        UrlGeneratorInterface::ABSOLUTE_URL
+                                    ), $crossSitePage->getSite()->getLocale());
+                                }
+                            }
+                        }
+
+                        $urls->addUrl($decoratedUrl, $site->getSlug());
+                    }
+                }
+            }
+        }
+    }
+}
