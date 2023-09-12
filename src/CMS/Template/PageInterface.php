6a86cc03cc31f7cc2ee18e36e4a22fec47a84314
Clement <clement@webetdesign.com>
2023-09-12 11:21:32 +0200

    feat: cms bredcrumb

--- src/CMS/Template/PageInterface.php
+++ src/CMS/Template/PageInterface.php
@@ -2,8 +2,11 @@
 
 namespace WebEtDesign\CmsBundle\CMS\Template;
 
-
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use WebEtDesign\CmsBundle\CMS\Configuration\RouteDefinition;
+use WebEtDesign\CmsBundle\Entity\CmsPage;
+use WebEtDesign\CmsBundle\Models\BreadcrumbItem;
 use WebEtDesign\CmsBundle\Vars\CmsVarsBag;
 
 interface PageInterface
@@ -11,4 +14,11 @@ interface PageInterface
     public function isSection(): bool;
 
     public function getRoute(): ?RouteDefinition;
+
+    public function buildBreadcrumbItem(
+        EntityManagerInterface $em,
+        Request                $request,
+        CmsPage                $page,
+        callable               $generateUrl,
+        string                 $defaultTitle): ?BreadcrumbItem;
 }
--- src/CMS/Template/SectionPage.php
+++ src/CMS/Template/SectionPage.php
@@ -2,7 +2,11 @@
 
 namespace WebEtDesign\CmsBundle\CMS\Template;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use WebEtDesign\CmsBundle\Attribute\AsCmsPage;
+use WebEtDesign\CmsBundle\Entity\CmsPage;
+use WebEtDesign\CmsBundle\Models\BreadcrumbItem;
 
 #[AsCmsPage(code: self::code)]
 class SectionPage extends AbstractPage
@@ -12,5 +16,14 @@ class SectionPage extends AbstractPage
     public bool $section = true;
 
     protected ?string $label = 'Admin section';
+
+    public function buildBreadcrumbItem(EntityManagerInterface $em,
+                                        Request                $request,
+                                        CmsPage                $page,
+                                        callable               $generateUrl,
+                                        string                 $defaultTitle): ?BreadcrumbItem
+    {
+        return null;
+    }
 
 }
