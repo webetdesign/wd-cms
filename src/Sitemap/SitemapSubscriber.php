12a210934828fd3167d59058a23144bfb30c6a87
Clement <clement@webetdesign.com>
2026-09-01 10:35:34 +0200

    fix sitemap multilingue generation

--- src/Sitemap/SitemapSubscriber.php
+++ src/Sitemap/SitemapSubscriber.php
@@ -95,13 +95,18 @@ class SitemapSubscriber implements EventSubscriberInterface
 
                             foreach ($page->getCrossSitePages() as $crossSitePage) {
                                 $crossRoute = $crossSitePage->getRoute();
-                                if ($crossRoute && !$crossRoute->isDynamic()) {
-                                    $decoratedUrl->addLink($this->urlGenerator->generate(
+                                if (!$crossSitePage->isActive() || !$crossRoute || $crossRoute->isDynamic()) {
+                                    continue;
+                                }
+
+                                $decoratedUrl->addLink(
+                                    $this->urlGenerator->generate(
                                         $crossRoute->getName(),
                                         [],
-                                        UrlGeneratorInterface::ABSOLUTE_URL
-                                    ), $crossSitePage->getSite()->getLocale());
-                                }
+                                        UrlGeneratorInterface::ABSOLUTE_URL,
+                                    ),
+                                    $crossSitePage->getSite()->getLocale(),
+                                );
                             }
                         }
 
