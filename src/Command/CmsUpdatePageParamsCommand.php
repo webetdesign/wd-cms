8fca9981b2596c9f2e71696b3d673186ce0cc4ce
Alexis <alexis@webetdesign.com>
2026-09-02 12:18:24 +0200

    Restore the Command import in the page params command

--- src/Command/CmsUpdatePageParamsCommand.php
+++ src/Command/CmsUpdatePageParamsCommand.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Component\Console\Attribute\Argument;
 use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Attribute\Option;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
